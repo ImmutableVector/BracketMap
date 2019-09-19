07dc4aad0506f4e44ba4f7042d908141b3de2fc6
Scott Peabody <scottpeabodyis@live.com>
2019-09-19 18:57:40 -0400

    updated to core rc1

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BracketMap
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
