46127e1e05ffb98a172d24a3f0fd3cd1a3872422
Vanessa Peixoto <Vanessa.Peixoto@datalinksoftware.com>
2019-09-19 20:51:07 -0400

    set up component

--- BracketMap.Web/src/app/models/tournament.ts
+++ BracketMap.Web/src/app/models/tournament.ts
@@ -1,5 +1,6 @@
-export interface Tournament {
-  id: string;
-  name: number;
-  playerCount: number;
+export class Tournament {
+  id: number | null = null;
+  name: string | null = null;
+  playerCount: number | null = null;
+  teamCount: number | null = null;
 }
