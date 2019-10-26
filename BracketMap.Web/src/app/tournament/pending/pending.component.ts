9f1d9fba37e2216beb1e6d5e4b56cdd5d853393d
Oz <jacob.osterhout@datalinksoftware.com>
2019-10-25 20:58:50 -0400

    part1

--- BracketMap.Web/src/app/tournament/pending/pending.component.ts
+++ BracketMap.Web/src/app/tournament/pending/pending.component.ts
@@ -7,16 +7,28 @@ import { ActivatedRoute } from '@angular/router';
   styleUrls: ['./pending.component.scss']
 })
 export class PendingComponent implements OnInit {
-  name?: string;
+  teamName: string | null = null;
+  name: string | null = null;
   id: number = +this.activatedRoute.snapshot.params.id;
 
   constructor(private activatedRoute: ActivatedRoute) { }
 
   ngOnInit() {
     // get some players have already signed up
+    // after that generate a randomName
   }
 
   addPlayer() {
     // tell the api that we have added a single player
+  }
+
+  private get randomName(): string {
+    const nameParts1 = ['Fast', 'Strong', 'Tough', 'Big', 'Tenacious', 'Awesome', 'Great'];
+    const nameParts2 = ['Tigers', 'Lions', 'Bears', 'Eagles', 'Dolphins', 'Horses', 'Ducks', 'Wolfs', 'Sharks'];
+    const namePart1 = nameParts1[Math.floor(Math.random() * nameParts1.length)];
+    const namePart2 = nameParts2[Math.floor(Math.random() * nameParts2.length)];
+    const name = `${namePart1} ${namePart2}`;
+    // check if name exists already
+    return name;
   }
 }
