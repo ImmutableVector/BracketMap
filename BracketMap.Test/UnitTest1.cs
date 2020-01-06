b47c776ddbddb2af8692c42746efddaa0575893d
Scott Peabody <scottpeabodyis@live.com>
2020-01-05 22:14:11 -0500

    ✨ Init NUnit project for unit testing in BracketMap

---
+++ BracketMap.Test/UnitTest1.cs
@@ -0,0 +1,18 @@
+using NUnit.Framework;
+
+namespace BracketMap.Test
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
